39b2e9f0f6d19267886b61149a3296f7451c3e1f
Agustin <agustin.avellaneda20@gmail.com>
2023-03-16 13:05:29 -0300

    modified:   Ruby-Api-Playground/Tarea para taringa/SIngleClient.rb

--- Ruby-Api-Playground/Tarea para taringa/SIngleClient.rb
+++ Ruby-Api-Playground/Tarea para taringa/SIngleClient.rb
@@ -3,7 +3,6 @@ require "net/http"
 require "openssl"
 require "pp"
 require "json"
-require "httparty"
 
 #gets single client by UUID
 def get_single_client(uuid)
@@ -22,29 +21,27 @@ def get_single_client(uuid)
 end
 
 def update_client(uuid)
-  api_key = "f124388a-43bb-4059-a605-f54453a3d4d1"
-  api_url = "https://api.cloud.wispro.co/v1/clients/#{uuid}"
+  url = URI("https://www.cloud.wispro.co/api/v1/clients/#{uuid}")
 
-  response = HTTParty.put(api_url, {
-    headers: {
-      "Authorization" => "Bearer #{api_key}",
-      "Content-Type" => "application/json",
-    },
-    body: {
-      name: "Agustin andres Avellaneda",
-      email: "agustin.avellaneda20@gmail.com",
-      phone: "1234567890",
-    }.to_json,
-  })
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
 
-  if response.success?
-    puts "Client updated successfully!"
-  else
-    puts "Error updating client: #{response.code} - #{response.body}"
-  end
+  request = Net::HTTP::Put.new(url)
+  request["accept"] = "application/json"
+  request["Authorization"] = "f124388a-43bb-4059-a605-f54453a3d4d1"
+  request.body = {
+    "name": "Agustin Andres Avellaneda",
+    'email': "Agustin.avellaneda20@gmail.com",
+    'phone': "2614676938",
+  }.to_json
+
+  response = http.request(request)
+  updated_client = JSON.parse(response.body)
+  puts updated_client
 end
+
+update_client("7501d2e7-9382-4117-8af0-3894481d49ea")
 
 get_single_client("7501d2e7-9382-4117-8af0-3894481d49ea")
 20.times { print "- - - - " }
 puts
-update_client("7501d2e7-9382-4117-8af0-3894481d49ea")
